2e27812cc3fdf25089eb42c74b024aa011ea5a9a
andrewyu55 <ay10118352@gmail.com>
2025-09-29 13:17:02 -0700

    created the basic structure with minimal capablities, currently working on parsing

---
+++ build/CMakeFiles/db-prototype.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/andrewyu/db-prototype/src/database.cpp" "CMakeFiles/db-prototype.dir/src/database.cpp.o" "gcc" "CMakeFiles/db-prototype.dir/src/database.cpp.o.d"
+  "/Users/andrewyu/db-prototype/src/main.cpp" "CMakeFiles/db-prototype.dir/src/main.cpp.o" "gcc" "CMakeFiles/db-prototype.dir/src/main.cpp.o.d"
+  "/Users/andrewyu/db-prototype/src/row.cpp" "CMakeFiles/db-prototype.dir/src/row.cpp.o" "gcc" "CMakeFiles/db-prototype.dir/src/row.cpp.o.d"
+  "/Users/andrewyu/db-prototype/src/table.cpp" "CMakeFiles/db-prototype.dir/src/table.cpp.o" "gcc" "CMakeFiles/db-prototype.dir/src/table.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/db-prototype.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/db-prototype.dir/src/database.cpp.o"
+  "CMakeFiles/db-prototype.dir/src/database.cpp.o.d"
+  "CMakeFiles/db-prototype.dir/src/main.cpp.o"
+  "CMakeFiles/db-prototype.dir/src/main.cpp.o.d"
+  "CMakeFiles/db-prototype.dir/src/row.cpp.o"
+  "CMakeFiles/db-prototype.dir/src/row.cpp.o.d"
+  "CMakeFiles/db-prototype.dir/src/table.cpp.o"
+  "CMakeFiles/db-prototype.dir/src/table.cpp.o.d"
+  "db-prototype"
+  "db-prototype.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/db-prototype.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
